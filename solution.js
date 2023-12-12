f2d0775079b38d5fa4ea7b13bf3e52959e2dda1a
Christine Marchese <chm978@gmail.com>
2023-12-11 22:13:27 -0500

    lab

--- solution.js
+++ solution.js
@@ -3,39 +3,72 @@ const { nums, words } = require("./data/data.js");
 // Every
 const isEveryNumGreaterThan2 = () => {
   //
+
+
+return nums.every((element) => {
+   return element.length > 2
+
+})
+
 };
+
+
+   
+
+
 
 const isEveryWordShorterThan7 = () => {
   //
-};
+  return words.every(element => element.length < 7);
+}
+
+  
+
 
 // Filter
 
 const arrayLessThan5 = () => {
   //
-};
+  
+  return nums.filter(element => element < 5);
+
+     }
+  
+
+  
+
 
 const arrayOddLengthWords = () => {
   //
+
+  return words.filter(element => element.length % 2 === 1);
 };
 
 // Find
 
 const firstValDivisibleBy4 = () => {
   //
-};
 
+  return nums.find(element => element % 4 === 0);
+
+}
 const firstWordLongerThan4Char = () => {
+  return words.find(element => element.length > 4);
+}
   //
-};
+
 
 // Find Index
 
 const firstNumIndexDivisibleBy3 = () => {
+
+  return nums.findIndex(element => element % 3 === 0);
   //
 };
 
 const firstWordIndexLessThan2Char = () => {
+
+  return words.findIndex(element => element.length < 2);
   //
 };
 
@@ -43,9 +76,12 @@ const firstWordIndexLessThan2Char = () => {
 
 const logValuesTimes3 = () => {
   //
+  return nums.forEach(element => element * 3);
 };
 
 const logWordsWithExclamation = () => {
+
+  return words.forEach(element => element === '!');
   //
 };
 
@@ -53,20 +89,28 @@ const logWordsWithExclamation = () => {
 
 const arrayValuesSquaredTimesIndex = () => {
   //
+
+  return nums.map((element, index) => element ** 2 * index);
 };
 
 const arrayWordsUpcased = () => {
+
+  return words.map(element => element.toUpperCase());
   //
 };
 
 // Some
 
 const areSomeNumsDivisibleBy7 = () => {
+
+  return nums.some(element => element % 7 === 0);
   //
 };
 
 const doSomeWordsHaveAnA = () => {
-  //
+
+  return words.some(element => element.includes('a'));
+  
 };
 
 module.exports = {
